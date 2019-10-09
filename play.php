57fc415e75e187d393f2088492b6c48ab9c2ce88
Eckerle.Andreas <eckerle@pearl.de>
2019-10-09 19:35:37 +0200

    added static strategy

--- play.php
+++ play.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once ('StaticStrategy.php');
 require_once ('Martingale.php');
 require_once ('ReverseMartingale.php');
 require_once ('Fibonacci.php');
@@ -7,27 +8,30 @@ require_once ('Fibonacci.php');
 use App\Fibonacci;
 use App\Martingale;
 use App\ReverseMartingale;
+use App\StaticStrategy;
 
-$gamer = array (new Martingale(), new ReverseMartingale(), new Fibonacci());
+$gamer = array (new StaticStrategy(), new Martingale(), new ReverseMartingale(), new Fibonacci());
 //echo "Money: ".$gamer->getMoney();
-echo "<table><tr><th>Martingale</th><th>Reverse Martingale</th><th>Fibonacci</th></tr>";
+echo "<table><tr><th>Static</th><th>Martingale</th><th>Reverse Martingale</th><th>Fibonacci</th></tr>";
 for ($i=0; $i<1000; $i++) {
-    if ($gamer[0]->nextRound() | $gamer[1]->nextRound() | $gamer[2]->nextRound()) {
+    if ($gamer[0]->nextRound() | $gamer[1]->nextRound() | $gamer[2]->nextRound() | $gamer[3]->nextRound()) {
         try {
             if (random_int(1, 100) <= 45) {
                 $gamer[0]->win();
                 $gamer[1]->win();
                 $gamer[2]->win();
+                $gamer[3]->win();
             } else {
                 $gamer[0]->loose();
                 $gamer[1]->loose();
                 $gamer[2]->loose();
+                $gamer[3]->loose();
             }
         } catch (Exception $e) {
             echo "random_int function throws the following exception " . $e;
             exit(1);
         }
-        echo "<tr><td>".$gamer[0]->getMoney()."</td><td>".$gamer[1]->getMoney()."</td><td>".$gamer[2]->getMoney()."</td></tr>";
+        echo "<tr><td>".$gamer[0]->getMoney()."</td><td>".$gamer[1]->getMoney()."</td><td>".$gamer[2]->getMoney()."</td><td>".$gamer[3]->getMoney()."</td></tr>";
     } else {
         break;
     }
@@ -36,3 +40,4 @@ echo "</table>";
 echo "Money: ".$gamer[0]->getMoney();
 echo "Money: ".$gamer[1]->getMoney();
 echo "Money: ".$gamer[2]->getMoney();
+echo "Money: ".$gamer[3]->getMoney();
